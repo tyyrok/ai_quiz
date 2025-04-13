7ad58e54ae522c88f14f48b7033dfa0a417c9db3
Pavel Zubko <tyyrok@gmail.com>
2025-04-13 14:37:37 +0400

    adjust template, remove unneccessary stuff

--- db/db.go
+++ db/db.go
@@ -20,25 +20,4 @@ func InitDB(dbpool *pgxpool.Pool) {
 	if err != nil {
 		log.Fatalf("Error with accessing/creating table %s\n", err)
 	}
-	// Test data
-	/*
-	var question_id int
-	err = dbpool.QueryRow(
-		context.Background(), `INSERT INTO questions (title) VALUES ($1) RETURNING id`, "What animal is the fastest?",
-	).Scan(&question_id)
-	if err != nil {
-		log.Fatalf("Error with adding question %s\n", err)
-	}
-	answers := []string{"Elephant", "Turtle", "Dog", "Gepard"}
-	for i := 0; i < 4; i++ {
-		_, err = dbpool.Exec(
-			context.Background(),
-			InsertAnswer,
-			answers[i], question_id,
-		)
-		if err != nil {
-			log.Fatalf("Error with adding answers %s\n", err)
-		}
-	}
-	*/
 }
--- db/queries.go
+++ db/queries.go
@@ -25,17 +25,3 @@ CREATE TABLE IF NOT EXISTS answers (
 );
 CREATE INDEX IF NOT EXISTS ix_answers_question_id ON answers (question_id);
 `
-
-
-const InsertQuestion = `
-INSERT INTO questions (title) VALUES ($1) RETURNING id
-`
-const InsertAnswer = `
-INSERT INTO answers (title, question_id) VALUES ($1, $2) RETURNING id
-`
-
-
-const SelectQuestionWithAnswers = `
-SELECT questions.id, questions.title, questions.likes, questions.dislikes
-FROM questions WHERE questions.date = $1;
-`
